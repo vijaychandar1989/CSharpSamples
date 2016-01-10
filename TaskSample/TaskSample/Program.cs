948f93d189bdfc9448a7e8fb1dbd1c95a86a5d6c
vijaychandar1989 <vijaychandar1989@gmail.com>
2016-01-10 22:38:02 +0530

    TasksBasicSample
    A sample application to demonstrate working of Tasks

---
+++ TaskSample/TaskSample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0, j = 0;
+           Task t1= Task.Run(() =>
+            {
+               
+                while (i<=10)
+                {
+                    Console.WriteLine("Thread 1 " + i++);
+                    Task.Delay(1000);
+                }
+
+            });
+           Task t2= Task.Run(() =>
+            {
+              
+                while (j <= 50)
+                {
+                    Console.WriteLine("Thread 2 " +j++);
+                    Task.Delay(100);
+                }
+
+
+            });
+
+            t1.Wait();
+            t2.Wait();
+            Console.WriteLine("The output is i=>" + i + " j=> " + j);
+
+            Console.Read();
+        }
+    }
+}
